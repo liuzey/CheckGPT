490f063165b22cd89dcbd701b4585fba89f7beb7
liuzey <1601007806@qq.com>
2023-06-08 21:50:46 -0500

    6th commit

--- CheckGPT/features.py
+++ CheckGPT/features.py
@@ -6,49 +6,59 @@ import json
 import h5py
 import time
 import argparse
+import torch.nn.functional as F
 
 
 parser = argparse.ArgumentParser(description='Demo of argparse')
 parser.add_argument('domain', type=str)
 parser.add_argument('brief', type=str)
 parser.add_argument('task', type=str)
+parser.add_argument('--number', type=int, default=0)
 args = parser.parse_args()
 
 domain = args.domain
 brief = args.brief
 task = args.task
-roberta = torch.hub.load('pytorch/fairseq', 'roberta.large').cuda()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+roberta = torch.hub.load('pytorch/fairseq', 'roberta.large').to(device)
 roberta.eval()
 
-with open("../GPABenchmark/{}_Task{}/{}_gpt.json".format(brief, task, brief), 'r') as f1:
+with open("../GPABenchmark/{}_TASK{}/gpt.json".format(brief, task, brief), 'r') as f1:
     data1 = json.load(f1)
+f1.close()
 
-with open("../GPABenchmark/{}_Task{}/{}_ground.json".format(brief, task, brief), 'r') as f2:
+with open("../GPABenchmark/{}_TASK{}/hum.json".format(brief, task, brief), 'r') as f2:
     data2 = json.load(f2)
+f2.close()
 
 print(len(list(data1.keys())))
 print(len(list(data2.keys())))
-
-total_length = len(list(data1.keys())) + len(list(data2.keys()))
+too_long = 0
+total_length = args.number if args.number else len(list(data1.keys())) + len(list(data2.keys()))
 
 start = time.time()
-data = h5py.File('./embeddings/{}{}.h5'.format(brief, task), 'w')
-data.create_dataset('data', (total_length, 512, 1024), dtype='f2')
+data = h5py.File('./embeddings/{}{}_{}.h5'.format(brief, task, total_length), 'w')
+data.create_dataset('data', (total_length, 512, 1024), dtype='f4')
 data.create_dataset('label', (total_length, 1), dtype='i')
 
-i = 0
-too_long = 0
-skip = 0
 
-for item in data1.values():
-    tokens = roberta.encode(item)
+def fetch_representation(text):
+    tokens = roberta.encode(text)
+    last_layer_features = None
 
-    if len(tokens) > 512:
+    if len(tokens) <= 512:
+        last_layer_features = roberta.extract_features(tokens)
+    return last_layer_features
+
+
+for gpt, hum in zip(data1.values(), data2.values()):
+    gpt_features, hum_features = fetch_representation(gpt), fetch_representation(hum)
+
+    if gpt_features is None or hum_features is None:
         too_long += 1
         continue
 
-    if i >= skip:
-        last_layer_features = roberta.extract_features(tokens)
         length = int(last_layer_features.shape[1])
 
         padding = torch.zeros(512, 1024, dtype=torch.float64)
--- CheckGPT/model.py
+++ CheckGPT/model.py
@@ -7,15 +7,14 @@ from transformers import RobertaTokenizer, RobertaModel, RobertaConfig, RobertaP
 
 
 class Attention(nn.Module):
-    def __init__(self, hidden_size, batch_first=False):
+    def __init__(self, hidden_size, batch_first=False, device="gpu"):
         super(Attention, self).__init__()
-
-        self.hidden_size = hidden_size
         self.batch_first = batch_first
-
-        self.att_weights = nn.Parameter(torch.Tensor(1, hidden_size), requires_grad=True)
-
+        self.hidden_size = hidden_size
         stdv = 1.0 / np.sqrt(self.hidden_size)
+        self.att_weights = nn.Parameter(torch.Tensor(1, hidden_size), requires_grad=True)
+        self.device = device
+
         for weight in self.att_weights:
             nn.init.uniform_(weight, -stdv, stdv)
 
@@ -28,32 +27,21 @@ class Attention(nn.Module):
         else:
             max_len, batch_size = inputs.size()[:2]
 
-        # apply attention layer
-        weights = torch.bmm(inputs,
-                            self.att_weights  # (1, hidden_size)
-                            .permute(1, 0)  # (hidden_size, 1)
-                            .unsqueeze(0)  # (1, hidden_size, 1)
-                            .repeat(batch_size, 1, 1)  # (batch_size, hidden_size, 1)
-                            )
+        weights = torch.bmm(inputs, self.att_weights.permute(1, 0).unsqueeze(0).repeat(batch_size, 1, 1))
         attentions = torch.softmax(F.relu(weights.squeeze()), dim=-1)
-        # create mask based on the sentence lengths
-        mask = torch.ones(attentions.size(), requires_grad=True).cuda()
-        # apply mask and renormalize attention scores (weights)
+        mask = torch.ones(attentions.size(), requires_grad=True)
+        if self.device == "gpu":
+            mask = mask.cuda()
         masked = attentions * mask
-        _sums = masked.sum(-1).unsqueeze(-1)  # sums per row
+        _sums = masked.sum(-1).unsqueeze(-1)
         attentions = masked.div(_sums)
-
-        # apply attention weights
         weighted = torch.mul(inputs, attentions.unsqueeze(-1).expand_as(inputs))
-        # get the final fixed vector representations of the sentences
-        representations = weighted.sum(1) #.squeeze()
-
-        return representations, attentions
+        return weighted.sum(1), attentions
 
 
 class AttenLSTM(nn.Module):
     def __init__(self, input_size=1024, hidden_size=256, batch_first=True, dropout=0.5, bidirectional=True,
-                 num_layers=2):
+                 num_layers=2, device="gpu"):
         super(AttenLSTM, self).__init__()
         self.dropout = nn.Dropout(dropout)
         self.lstm1 = nn.LSTM(input_size=input_size,
@@ -61,19 +49,19 @@ class AttenLSTM(nn.Module):
                              num_layers=1,
                              bidirectional=bidirectional,
                              batch_first=batch_first)
-        self.atten1 = Attention(hidden_size * 2, batch_first=batch_first)  # 2 is bidrectional
+        self.atten1 = Attention(hidden_size * 2, batch_first=batch_first, device=device)
         self.lstm2 = nn.LSTM(input_size=hidden_size * 2,
                              hidden_size=hidden_size,
                              num_layers=1,
                              bidirectional=bidirectional,
                              batch_first=batch_first)
-        self.atten2 = Attention(hidden_size * 2, batch_first=batch_first)
+        self.atten2 = Attention(hidden_size * 2, batch_first=batch_first, device=device)
         self.fc = nn.Linear(hidden_size * num_layers * 2, 2)
 
     def forward(self, x):
-        out1, (h_n, c_n) = self.lstm1(x)
-        x, _ = self.atten1(out1)  # skip connect
-        out2, (h_n, c_n) = self.lstm2(out1)
+        out1, (_, _) = self.lstm1(x)
+        x, _ = self.atten1(out1)
+        out2, (_, _) = self.lstm2(out1)
         y, _ = self.atten2(out2)
 
         z = torch.cat([x, y], dim=1)
